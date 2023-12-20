59ab9c227b6a20325fa3e4f790c4cd3ef289bbe9
dazcalifornia <divertzt@gmail.com>
2023-12-21 03:25:01 +0700

    Initial Commit

---
+++ TaskSystem/TaskSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskSystemApp.swift
+//  TaskSystem
+//
+//  Created by Supakit Wiwustrinon on 21/12/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct TaskSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
